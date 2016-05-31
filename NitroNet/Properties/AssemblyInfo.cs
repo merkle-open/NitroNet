af721af2d69ff585e110861b729dcc329803c713
fgeiger <fabian.geiger@namics.com>
2016-05-31 18:53:02 +0200

    NitroNet project was renamed to NitroNet.Core

--- NitroNet/Properties/AssemblyInfo.cs
+++ NitroNet/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NitroNet")]
+[assembly: AssemblyTitle("NitroNet.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NitroNet")]
+[assembly: AssemblyProduct("NitroNet.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
