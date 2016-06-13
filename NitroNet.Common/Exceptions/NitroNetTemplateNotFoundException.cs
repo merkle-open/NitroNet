102938dd5a0a1b48652b8a4ec7f5734c3d01fe86
fgeiger <fabian.geiger@namics.com>
2016-06-13 20:21:11 +0200

    Naming conventions applied

---
+++ NitroNet.Common/Exceptions/NitroNetTemplateNotFoundException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NitroNet.Common.Exceptions
+{
+    public class NitroNetTemplateNotFoundException : Exception
+    {
+        public NitroNetTemplateNotFoundException()
+        {
+        }
+
+        public NitroNetTemplateNotFoundException(string message)
+            : base(message)
+        {
+        }
+
+        public NitroNetTemplateNotFoundException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
